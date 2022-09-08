409dc24ec75de54857ec728e61c22e6623a62c4c
syorn96 <syorn2014@gmail.com>
2022-09-07 18:56:07 -0700

    finishes CR for dino db

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const db = require('./models')
 const dinoCRUD = async () => {
     try{
     // C
-        const newDino = await db.dino.create({
-            name: 'Saw-Disk',
-            type: 'Stegosaurus'
-        })
+        // const newDino = await db.dino.create({
+        //     name: 'Birdy',
+        //     type: 'Pterodactyl'
+        // })
+        // console.log(newDino)
     // R
+        // const allDinos = await db.dino.findAll()
+        // console.log(allDinos)
+        const someDinos = await db.dino.findAll({
+            where: {
+                name: 'T-Rex'
+            }
+        })
+        console.log(someDinos)
     // U
     // D
     } catch(err) {
