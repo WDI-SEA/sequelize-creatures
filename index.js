94f65f40e20dd344eb5e98cfeb2808fc7a7a42b3
syorn96 <syorn2014@gmail.com>
2022-09-07 19:08:47 -0700

    CRUD complete for DINO DB

--- index.js
+++ index.js
@@ -11,14 +11,36 @@ const dinoCRUD = async () => {
     // R
         // const allDinos = await db.dino.findAll()
         // console.log(allDinos)
-        const someDinos = await db.dino.findAll({
+        // const someDinos = await db.dino.findAll({
+        //     where: {
+        //         name: 'T-Rex'
+        //     }
+        // })
+        // console.log(someDinos)
+        // const [dino, created] = await db.dino.findOrCreate({
+        //     where: {
+        //         name: "Tyler"
+        //     },
+        //     defaults: {
+        //         type: 'ABB'
+        //     }
+        // })
+        // console.log(dino)
+        // console.log(`The dino was created: ${created}`)
+    // U
+        // const numRowsChanged = await db.dino.update({ type:'ASIAN BABY BOY'}, {
+        //     where: {
+        //         name: 'Tyler'
+        //     }
+        // })
+        // console.log(numRowsChanged)
+    // D
+        const numRowsDeleted = await db.dino.destroy({
             where: {
                 name: 'T-Rex'
             }
         })
-        console.log(someDinos)
-    // U
-    // D
+        console.log(numRowsDeleted)
     } catch(err) {
         console.warn(err)
     }
