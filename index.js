0b4c431140b2d55d790ed7ae539f7f3e9be5628e
valerieyang00 <valerieyang00@gmail.com>
2022-09-07 19:48:45 -0700

    finished deliverable

--- index.js
+++ index.js
@@ -3,51 +3,53 @@ const db = require("./models")
 const dinoCRUD = async () => {
     try {
         // Create
-    // const newDino = await db.dino.create({
-    //     name: "Petrie",
-    //     type:"pteranodon"
-    // })
+        // const newDino = await db.dino.create({
+        //     name: "Petrie",
+        //     type:"pteranodon"
+        // })
 
-    // // Find or Create
-    // const [dino, created] = await db.dino.findOrCreate({
-    //     where: {
-    //         name: "Spike"
-    //     },
-    //     defaults: {
-    //         type: "stegosaurus"
-    //     }
-    // })
+        // // Find or Create
+        // const [dino, created] = await db.dino.findOrCreate({
+        //     where: {
+        //         name: "Spike"
+        //     },
+        //     defaults: {
+        //         type: "stegosaurus"
+        //     }
+        // })
         //READ
-    //Find All
-    // const findAll = await db.dino.findAll()
-    // console.log(findAll)
-    //Find One
-    // const findOne = await db.dino.findOne({
-    //     where: {
-    //         name: "Ducky"
-    //     }
-    // })
-    // console.log(findOne)
+        //Find All
+        // const findAll = await db.dino.findAll()
+        // console.log(findAll)
+        //Find One
+        // const findOne = await db.dino.findOne({
+        //     where: {
+        //         name: "Ducky"
+        //     }
+        // })
+        // console.log(findOne)
         //Update
-    // const numRowsUpdated = await db.dino.update({
-    //     type: "saurolophus"}, {
-    //     where: {
-    //         name: "Ducky"
-    //     }
-    // })
-    // console.log(numRowsUpdated)
+        // const numRowsUpdated = await db.dino.update({
+        //     type: "saurolophus"}, {
+        //     where: {
+        //         name: "Ducky"
+        //     }
+        // })
+        // console.log(numRowsUpdated)
         //Destroy
-    
-    // const numRowsDeleted = await db.dino.destroy({
-    //     where: {
-    //         name: "Ducky"
-    //     }
-    // })
-    // console.log(numRowsDeleted)
-    console.log("dinoCRUD done")
 
-    } catch(err) {
+        // const numRowsDeleted = await db.dino.destroy({
+        //     where: {
+        //         name: "Ducky"
+        //     }
+        // })
+        // console.log(numRowsDeleted)
+
+    } catch (err) {
         console.warn(err)
+    }
+    finally {
+        console.log("dinoCRUD done")
     }
 }
 
@@ -56,51 +58,52 @@ dinoCRUD()
 const creatureCRUD = async () => {
     try {
         // Create
-    // const newCreature = await db.creature.create({
-    //     img_url: "https://cdn-images-1.medium.com/max/1200/1*a2VvYsKGApR-E1SnT5O7yQ.jpeg",
-    //     type:"mastodon"
-    // })
+        // const newCreature = await db.creature.create({
+        //     img_url: "https://cdn-images-1.medium.com/max/1200/1*a2VvYsKGApR-E1SnT5O7yQ.jpeg",
+        //     type:"mastodon"
+        // })
 
-    // // Find or Create
-    // const [creature, created] = await db.creature.findOrCreate({
-    //     where: {
-    //         type: "megalonyx"
-    //     },
-    //     defaults: {
-    //         img_url: "https://animalgeography.files.wordpress.com/2018/08/sloth-banner-e1535192925361.jpg?w=584&h=325"
-    //     }
-    // })
+        // // Find or Create
+        // const [creature, created] = await db.creature.findOrCreate({
+        //     where: {
+        //         type: "megalonyx"
+        //     },
+        //     defaults: {
+        //         img_url: "https://animalgeography.files.wordpress.com/2018/08/sloth-banner-e1535192925361.jpg?w=584&h=325"
+        //     }
+        // })
         //READ
-    //Find All
-    // const findAll = await db.creature.findAll()
-    // console.log(findAll)
-    //Find One
-    // const findOne = await db.creature.findOne({
-    //     where: {
-    //         type: "megalonyx"
-    //     }
-    // })
-    // console.log(findOne)
+        //Find All
+        // const findAll = await db.creature.findAll()
+        // console.log(findAll)
+        //Find One
+        // const findOne = await db.creature.findOne({
+        //     where: {
+        //         type: "megalonyx"
+        //     }
+        // })
+        // console.log(findOne)
         //Update
-    // const numRowsUpdated = await db.creature.update({
-    //     type: "giant beaver"}, {
-    //         where: {
-    //     img_url: "http://www.beringia.com/sites/default/files/Giant-Beaver-banner.jpg"
-    //     }
-    // })
-    // console.log(numRowsUpdated)
+        // const numRowsUpdated = await db.creature.update({
+        //     type: "giant beaver"}, {
+        //         where: {
+        //     img_url: "http://www.beringia.com/sites/default/files/Giant-Beaver-banner.jpg"
+        //     }
+        // })
+        // console.log(numRowsUpdated)
         //Destroy
-    
-    // const numRowsDeleted = await db.creature.destroy({
-    //     where: {
-    //         type: "giant beaver"
-    //     }
-    // })
-    // console.log(numRowsDeleted)
-    console.log("creatureCRUD done")
 
-    } catch(err) {
+        // const numRowsDeleted = await db.creature.destroy({
+        //     where: {
+        //         type: "giant beaver"
+        //     }
+        // })
+        // console.log(numRowsDeleted)
+
+    } catch (err) {
         console.warn(err)
+    } finally {
+        console.log("creatureCRUD done")
     }
 }
 
