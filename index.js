6065b6fd165c01b336fb680fb07acd978727fd17
tylerchan33 <tylerchan33@gmail.com>
2022-09-07 19:59:53 -0700

    delivered

--- index.js
+++ index.js
@@ -3,54 +3,57 @@ const db = require("./models")
 const dinoCrud = async () => {
     try {
         //  CREATE
-        // const newDino = await db.dino.create({
-        //     name: "Val",
-        //     type: "Velociraptor"
-        // })
+        const newDino = await db.dino.create({
+            name: "Val",
+            type: "Velociraptor"
+        })
         // READ
-        // const allDinos = await db.dino.findAll()
-        // const someDinos = await db.dino.findAll({
-        //     where: {
-        //         name: "Stephen"
-        //     }
-        // })
-        // console.log(allDinos)
-        // console.log(someDinos)
+        const allDinos = await db.dino.findAll()
+        const someDinos = await db.dino.findAll({
+            where: {
+                name: "Stephen"
+            }
+        })
+        console.log(allDinos)
+        console.log(someDinos)
         // UPDATE
-        // const numRowsChanged = await db.dino.update( {name: "Yas"}, {
-        //     where: {
-        //         name: "Yasmine"
-        //     }
-        // })
-        // console.log(numRowsChanged)
+        const numRowsChanged = await db.dino.update( {name: "Yas"}, {
+            where: {
+                name: "Yasmine"
+            }
+        })
+        console.log(numRowsChanged)
         // DESTROY
-        // const numRowsDeleted = await db.dino.destroy({
-        //     where: {
-        //         id: "2"
-        //     }
-        // })
-        // console.log(numRowsDeleted)
+        const numRowsDeleted = await db.dino.destroy({
+            where: {
+                id: "2"
+            }
+        })
+        console.log(numRowsDeleted)
     } catch(err) {
         console.warn(err)
     }
 }
 
+dinoCrud()
+
 const creatureCrud = async () => {
     try {
         // CREATE
-        // const newCreature = await db.creature.create({
-        //     img_url: "https://static.wikia.nocookie.net/villains/images/d/dc/006pa8xx.jpg/revision/latest?cb=20150524012819",
-        //     type: "Kreacher"
-        // })
+        const newCreature = await db.creature.create({
+            img_url: "https://static.wikia.nocookie.net/villains/images/d/dc/006pa8xx.jpg/revision/latest?cb=20150524012819",
+            type: "Kreacher"
+        })
         // READ
-        // const allCreatures = await db.creature.findAll()
-        // console.log(allCreatures)
+        const allCreatures = await db.creature.findAll()
+        console.log(allCreatures)
         // UPDATE
-        // const numRowsChanged = await db.creature.update( { img_url: "https://igws.indiana.edu/images/fossilsandtime/sabertooth.jpg"}, {
-        //     where: {
-        //         img_url: "https://static01.nyt.com/images/2022/05/17/science/07tb-sabertooth1/07tb-sabertooth1-videoSixteenByNine3000.jpg"
-        //     }
-        // } )
+        const numRowsChanged = await db.creature.update( { img_url: "https://igws.indiana.edu/images/fossilsandtime/sabertooth.jpg"}, {
+            where: {
+                img_url: "https://static01.nyt.com/images/2022/05/17/science/07tb-sabertooth1/07tb-sabertooth1-videoSixteenByNine3000.jpg"
+            }
+        }) 
+        console.log(numRowsChanged)
         // DELETE
         const numRowsDeleted = await db.creature.destroy({
             where: {
