309f0e76725f768ac25a83aa43d3b12815843cce
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-08 09:38:32 -0700

    finished homework walkalong

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const db = require('./models')
 
-db.dino.create({
-    id: '1',
-    name: 'Spinosaurus',
-    type: 'S. aegyptiacus',
-}).then(createdUser=>{
-    // the create promise returns the
-    // new row of data that has been created
-    // (otherwise it throws an error)
-    console.log(createdUser)
-    // terminates the node process at this point so that we don't have to force-quit
-    process.exit()
-})
+const CRUD =asunc () => {
+    try {
+        //create
+        //make new dino
+        // await db.dino.create({
+        //     name: 'Spinosaurus',
+        //     type: 'Spinosaurus aegyptiacus'
+        // })
+        // await db.creature.create({
+        //     img_url:'https://i1.sndcdn.com/avatars-000495996573-7xpt1u-t500x500.jpg'
+        //     type: 'hecking chonker'
+        // })
+        //read
+        // const dino = await db.dino.findOne({
+        //     where: {
+        //         name: 'Spinosaurus'
+        //     }
+        // })
+        // console.log('Spinosaurus:', dino)
+        // const creature = await db.creature.findByPk(1)
+        // console.log('creature:', creature)
+        //update
+        // await db.dino.update({ name: 'Spinosaurus'}, {
+        //     where: {
+        //         id:1
+        //     }
+        // })
+        // await db.creature.update({ type: 'Super Heckin Chonker' }, {
+        //     where: {
+        //         type: 'Heckin Chonker'
+        //     }
+        // })
+        // const chonkers = db.creature.findAll()
+        // console.log('chonkers', chonkers)
+        //destroy
+        // await db.creature.destroy({
+        //     where: {
+        //         id: 2
+        //     }
+        // })
+
+    }
+
+} catch (err) {
+    console.log(err)
+}
+
+CRUD()
