b3d94031cb12a078dc0e2b830e153836a2794319
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-08 08:47:25 -0700

    partial homework

---
+++ seeders/20220908051515-creature.js
@@ -0,0 +1,16 @@
+// 'use strict';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//       return queryInterface.bulkInsert('Dinosaurs', [{
+//         name: 'General Dino',
+//         type: 'Assemblasaurus',
+//         createdAt: new Date(),
+//         updatedAt: new Date()
+//       }], {});
+//   },
+
+//   down: (queryInterface, Sequelize) => {
+//       return queryInterface.bulkDelete('Dinosaurs', null, {});
+//   }
+// };
