1503fe1826b408f656fae7b8eb74dbfc764ba865
jtw007 <joshwu095@gmail.com>
2022-12-15 21:25:17 -0800

    mvp finished

--- index.js
+++ index.js
@@ -3,44 +3,120 @@ const db = require("./models")
 
 //---CREATE DINOS FUNCTION----
 async function findOrCreateDino(nameInput, typeInput) {
-  try {
-    let foundDino = await db.dino.findOrCreate({
-      where: {
-        name: nameInput,
-        type: typeInput
-      }
-    })
-    console.log(foundDino)
-  } catch (error) {
-    console.error(error.message)
-  }
+    try {
+        let foundDino = await db.dino.findOrCreate({
+            where: {
+                name: nameInput,
+                type: typeInput
+            }
+        })
+        // console.log(foundDino)
+    } catch (error) {
+        console.error(error.message)
+    }
 }
-
-// findOrCreateDino("rawr", "xd")
+// findOrCreateDino()
 
 
 // --READ --returns array of all dinos
 async function getAllDinos() {
-    try{
+    try {
         const allDinos = await db.user.findAll()
-        console.log(allDinos)
+        // console.log(allDinos)
     } catch (error) {
         console.log(error.message)
     }
 }
-
-
-// console.log(getAllDinos()) 
+//getAllDinos() 
 
 // --UPDATE --
-async function updateDino(nameInput,typeInput) {
-    try{
-        const numRowsChanged = await db.dino.update({
-            name: nameInput,
-            type: typeInput
-        }) 
-    } catch(error) {
+async function updateDino(idInput, nameInput, typeInput) {
+    try {
+        const updatedDino = await db.dino.update(
+            { name: nameInput, type: typeInput },
+            {
+                where: {
+                    id: idInput,
+                }
+            })
+        // console.log(updatedDino) 
+    } catch (error) {
         console.log(error.message)
     }
 }
-// --DESTROY--
+// updateDino()
+
+// --DESTROY-- delete a dino 
+async function deleteDino(idInput) {
+    try {
+        let deletedDino = await db.dino.destroy({
+            where: {
+                id: idInput
+            }
+        })
+        // console.log(deletedDino)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+// deleteDino()
+
+
+// ----CREATURES------
+//---CREATE---
+async function createCreature(imgInput, typeInput) {
+    try {
+        let creature = await db.creature.findOrCreate({
+            where: {
+                img_url: imgInput,
+                type: typeInput
+            }
+        })
+        console.log(creature)
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+// createCreature()
+
+//---READ ALL---
+async function allCreatures() {
+    try {
+        let allCreatures = await db.creature.findAll()
+        console.log(allCreatures)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+// allCreatures()
+
+//---UPDATE---
+async function updateCreature(idInput, imgInput, typeInput) {
+    try {
+        let updateCreature = await db.creature.update(
+            { img_url: imgInput, type: typeInput },
+            {
+                where: {
+                    id: idInput,
+                }
+            })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+// updateCreature(1, 'google.com', 'monster')
+
+//---DESTROY/DELETE---
+async function deleteCreature(idInput) {
+    try {
+        let deleteCreature = await db.creature.destroy({
+            where: {
+                id: idInput,
+            }
+        })
+        console.log(deleteCreature)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+// deleteCreature(1)
