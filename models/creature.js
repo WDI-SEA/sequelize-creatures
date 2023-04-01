9c3d0d32c672e24c099a22edc6ca37fb1a5ad068
andrewbantly <andrewbantly@gmail.com>
2023-04-01 14:52:48 -0700

    deliverable complete

---
+++ models/creature.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class creature extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  creature.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    img_url: DataTypes.STRING,
+    type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'creature',
+  },);
+  return creature;
+};
