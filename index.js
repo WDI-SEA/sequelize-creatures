a6612ff872da307b69dd615e3cac0470cb7cdc8c
juanedcabrera <juanedcabrera@gmail.com>
2023-04-01 20:53:47 -0600

    Creatures CRUD updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const creatures = require('./models')
+const creatures = require("./models");
 // const { Sequelize } = require("sequelize");
 
 // // https://sequelize.org/docs/v6/getting-started/
@@ -9,7 +9,6 @@ const creatures = require('./models')
 
 // // https://sequelize.org/docs/v6/core-concepts/model-basics/#using-sequelizedefine
 // // https://sequelize.org/docs/v7/other-topics/other-data-types/
-
 
 // const Dino = sequelize.define("dino", {
 //   // Model attributes are defined here
@@ -22,60 +21,123 @@ const creatures = require('./models')
 //   type: Sequelize.TEXT,
 // });
 
+const createDino = async () => {
+  try {
+    const dino = await db.dino.create({
+      name: "Tyrannosaurus Rex",
+      type: "carnivorous",
+    });
+    console.log(`ID is ${dino.id}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+createDino();
 
-const createDino = async() => {
-    try {
-        const dino = await db.dino.create({
-            name: 'Tyrannosaurus Rex',
-            type: 'carnivorous'
-        })
-        console.log(`ID is ${dino.id}`)
-    } catch (err) {
-        console.log(err)
+const readDino = async () => {
+  try {
+    const dino = await db.dino.findOne({
+      where: { name: "Tyrannosaurus Rex" },
+    });
+    if (dino) {
+      console.log(`found ${dino.id}`);
+    } else {
+      console.log(`not found ${id}`);
     }
-}
-createDino()
+  } catch (error) {
+    console.log(error);
+  }
+};
+readDino();
 
-const readDino = async() => {
-    try {
-        const dino = await db.dino.findOne({
-            where: { name: "Tyrannosaurus Rex"}
-        })
-            if (dino) {
-                console.log(`found ${dino.id}`)
-            } else {
-                console.log(`not found ${id}`)
-            }
-        } catch (error) {
-            console.log(error)
-    } 
-    }
-readDino()
-
-const updateDino = async() => {
-    try {
-       const numRowsChanged = await db.dino.update({
-        name: 'Tyrannosaurus Rex',
-       }, {
+const updateDino = async () => {
+  try {
+    const numRowsChanged = await db.dino.update(
+      {
+        name: "Tyrannosaurus Rex",
+      },
+      {
         where: {
-            type:'carnivorous'
-        }
-       })
-        } catch (error) {
-            console.log(error)
-        }
-    }
-updateDino()
+          type: "carnivorous",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+updateDino();
 
 const deleteDino = async () => {
-    try {
-      const numRowDestroyed = await db.dino.destroy({
-        where: {
-          type: 'carnivorous',
-        },
-      })
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    const numRowDestroyed = await db.dino.destroy({
+      where: {
+        type: "carnivorous",
+      },
+    });
+  } catch (error) {
+    console.log(error);
   }
-deleteDino()
+};
+deleteDino();
+
+const createCreature = async () => {
+  try {
+    const creature = await db.creature.create({
+      img_url: "https://image.com/image.png",
+      type: "carnivorous",
+    });
+    console.log(`creature ${creature.id}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+createCreature();
+
+const readCreature = async () => {
+  try {
+    const creature = await db.creature.findOne({
+      where: { type: "carnivorous" },
+    });
+    if (creature) {
+      console.log(`found ${creature.id}`);
+    } else {
+      console.log(`not found ${id}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+readCreature();
+
+const updateCreature = async () => {
+  try {
+    const numRowsChanged = await db.creature.update(
+      {
+        img_url: "https://image.com/image.png",
+      },
+      {
+        where: {
+          type: "carnivorous",
+        },
+      }
+    );
+    console.log(`${numRowsChanged} rows updated.`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+updateCreature();
+
+const deleteCreature = async () => {
+  try {
+    const numRowsDestroyed = await db.creature.destroy({
+      where: {
+        type: "carnivorous",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+deleteCreature();
