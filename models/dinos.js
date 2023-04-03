4094c8764866f0aeea306661e04d7a1eb1ef5de9
Powwow84 <pao.saelee84@gmail.com>
2023-04-02 19:58:08 -0700

    Pao S sequelize creatures

---
+++ models/dinos.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class dinos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  dinos.init({
+    name: DataTypes.TEXT,
+    type: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'dinos',
+  });
+  return dinos;
+};
